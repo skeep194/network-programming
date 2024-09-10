a93e9606e21d169666d574de043b886b4e9797bf
JongunJeong <skeep194@knu.ac.kr>
2024-09-11 03:03:40 +0900

    add template

---
+++ template/template_udp_client.c
@@ -0,0 +1,47 @@
+// 2019113928 정종운
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <time.h>
+
+void error_handling(char *message);
+void process(int sock)
+{
+}
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    socklen_t adr_sz;
+
+    struct sockaddr_in serv_adr, from_adr;
+    if (argc != 3)
+    {
+        printf("Usage : %s <IP> <port>\n", argv[0]);
+        exit(1);
+    }
+
+    sock = socket(PF_INET, SOCK_DGRAM, 0);
+    if (sock == -1)
+        error_handling("socket() error");
+
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_adr.sin_port = htons(atoi(argv[2]));
+    // sendto, recvfrom
+    process(sock);
+    // close socket
+    close(sock);
+    return 0;
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
